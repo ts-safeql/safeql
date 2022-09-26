252bd3404f9bc74d5c8c2ff1564bdb333e975e79
Karl Horky <karl.horky@gmail.com>
2022-09-26 18:39:32 +0200

    Add Postgres.js interpolation example
    Ref: #56

--- demos/postgresjs-demo/src/index.ts
+++ demos/postgresjs-demo/src/index.ts
@@ -1,7 +1,8 @@
 import postgres from "postgres";
 
-export async function check() {
+export async function check(id: number) {
   const sql = postgres();
 
   sql<{ id: number }[]>`SELECT id FROM person`;
+  sql<{ id: number }[]>`SELECT id FROM person WHERE id = ${id}`;
 }
