63bddfababc8b59f22fe4d573399ad8b730c679e
Karl Horky <karl.horky@gmail.com>
2022-10-02 09:15:31 +0200

    Add Postgres.js interpolation and type alias examples (#59)

--- demos/postgresjs-demo/src/index.ts
+++ demos/postgresjs-demo/src/index.ts
@@ -1,7 +1,10 @@
 import postgres from "postgres";
 
-export async function check() {
+export async function check(id: number) {
   const sql = postgres();
 
   sql<{ id: number }[]>`SELECT id FROM person`;
+
+  type Person = { id: number };
+  sql<Person[]>`SELECT id FROM person WHERE id = ${id}`;
 }
