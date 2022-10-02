430c9c46cec7d1c1922b4cf20f78630d0fab413c
Newbie012 <co.eliya2@gmail.com>
2022-10-02 09:46:15 +0300

    prettier

--- demos/big-project/src/original.ts
+++ demos/big-project/src/original.ts
@@ -10,6 +10,6 @@ export function check() {
   const sql = postgres();
 
   sql<{ id: number }[]>`SELECT id FROM starship`;
-  sql<{ id: number; name: string; captain_id: number | null; }[]>`SELECT * FROM starship`;
+  sql<{ id: number; name: string; captain_id: number | null }[]>`SELECT * FROM starship`;
   sql<Starship[]>`SELECT * FROM starship`;
 }
