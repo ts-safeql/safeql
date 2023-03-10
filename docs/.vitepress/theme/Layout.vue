26c3ebb3186ecd0a87f8fe734ebf9ef63116b952
Karl Horky <karl.horky@gmail.com>
2023-03-10 11:14:50 +0100

    Improve responsive design
    Co-authored-by: Divyansh Singh <40380293+brc-dd@users.noreply.github.com>

--- docs/.vitepress/theme/Layout.vue
+++ docs/.vitepress/theme/Layout.vue
@@ -5,7 +5,23 @@ import DefaultTheme from "vitepress/theme";
 <template>
   <DefaultTheme.Layout>
     <template #home-hero-image>
-      <video src="/video.mp4" autoplay loop muted />
+      <video autoplay loop muted src="/video.mp4" class="video" />
     </template>
   </DefaultTheme.Layout>
 </template>
+
+<style scoped>
+.video {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  max-height: 192px;
+  transform: translate(-50%, -50%);
+}
+
+@media (min-width: 640px) {
+  .video {
+    max-height: unset;
+  }
+}
+</style>
