5fd52062e0ff6e783aee1fec4b329f0e737f1263
Karl Horky <karl.horky@gmail.com>
2023-03-10 11:54:15 +0100

    Add video to homepage hero (#26)
    * Add video to homepage hero
    
    * Add Layout with video
    
    * Update version
    
    * Update lockfile
    
    * Fix text overlapping with video
    
    * Autoplay, loop and mute video
    
    * Improve responsive design
    
    Co-authored-by: Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Divyansh Singh <40380293+brc-dd@users.noreply.github.com>

---
+++ docs/.vitepress/theme/Layout.vue
@@ -0,0 +1,27 @@
+<script setup>
+import DefaultTheme from "vitepress/theme";
+</script>
+
+<template>
+  <DefaultTheme.Layout>
+    <template #home-hero-image>
+      <video autoplay loop muted src="/video.mp4" class="video" />
+    </template>
+  </DefaultTheme.Layout>
+</template>
+
+<style scoped>
+.video {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  max-height: 192px;
+  transform: translate(-50%, -50%);
+}
+
+@media (min-width: 640px) {
+  .video {
+    max-height: unset;
+  }
+}
+</style>
