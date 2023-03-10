49ce9fdf211fb2153f2dd64c129a3a3ec3c9d527
Karl Horky <karl.horky@gmail.com>
2023-03-10 11:12:00 +0100

    Autoplay, loop and mute video

--- docs/.vitepress/theme/Layout.vue
+++ docs/.vitepress/theme/Layout.vue
@@ -5,7 +5,7 @@ import DefaultTheme from "vitepress/theme";
 <template>
   <DefaultTheme.Layout>
     <template #home-hero-image>
-      <video src="/video.mp4" />
+      <video src="/video.mp4" autoplay loop muted />
     </template>
   </DefaultTheme.Layout>
 </template>
