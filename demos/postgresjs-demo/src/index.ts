997ef7cdca848c1e6429b27d68b80df3d006bb2a
Newbie012 <co.eliya2@gmail.com>
2023-03-17 12:18:18 +0200

    add example

--- demos/postgresjs-demo/src/index.ts
+++ demos/postgresjs-demo/src/index.ts
@@ -4,6 +4,7 @@ export async function check(id: number) {
   const sql = postgres();
 
   sql<{ id: number }[]>`SELECT id FROM person`;
+  sql<{ mood: "sad" | "ok" | "happy" }[]>`SELECT mood FROM person`;
 
   type Person = { id: number };
   sql<Person[]>`SELECT id FROM person WHERE id = ${id}`;
