abf643a5e51cdcf290cc5e0b343a17d23cb11897
Newbie012 <co.eliya2@gmail.com>
2024-04-30 22:56:15 +0300

    cleanup

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,6 +1,3 @@
-import TwoslashFloatingVue from "@shikijs/vitepress-twoslash/client";
-import "@shikijs/vitepress-twoslash/style.css";
-import type { EnhanceAppContext } from "vitepress";
 import Theme from "vitepress/theme";
 import Layout from "./Layout.vue";
 import "./custom.css";
@@ -8,8 +5,7 @@ import "./custom.css";
 export default {
   extends: Theme,
   Layout: Layout,
-  async enhanceApp({ app }: EnhanceAppContext) {
-    app.use(TwoslashFloatingVue);
+  async enhanceApp() {
     import("@vercel/analytics").then(({ default: analytics }) => {
       analytics.inject();
     });
