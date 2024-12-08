0e06bea0d9b443263c90b66ed1c80a347944d6ff
Eliya Cohen <eliya@medflyt.com>
2024-12-08 08:59:45 +0200

    update snapshot

--- packages/eslint-plugin/src/utils/query-context.test.ts
+++ packages/eslint-plugin/src/utils/query-context.test.ts
@@ -125,6 +125,8 @@ describe("getQueryContext", () => {
     expect(getQueryContext(query)).toMatchInlineSnapshot(`
       [
         "SELECT",
+        "WHEN",
+        "WHEN",
         "FROM",
       ]
     `);
