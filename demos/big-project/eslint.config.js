d90faccb8e3d04dbb27d5648e4e9bd9587a1b810
Eliya Cohen <co.eliya2@gmail.com>
2025-03-25 23:04:01 +0200

    fix

--- demos/big-project/eslint.config.js
+++ demos/big-project/eslint.config.js
@@ -4,7 +4,7 @@ import tseslint from "typescript-eslint";
 import safeql from "@ts-safeql/eslint-plugin/config";
 
 export default tseslint.config({
-  files: ["src/**/*.ts"],
+  files: ["src/**/*.ts", "_fake_src/**/*.ts"],
   languageOptions: {
     parser: tseslint.parser,
     parserOptions: {
